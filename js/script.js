e7ac3207cfc0aacad02ebab689a3052f03afbdc6
Daniel David <daniel.david@students.ptechnyc.org>
2018-01-31 23:18:29 +0000

    Got to display 'myPlaylist' just have to make it an actual link

--- js/script.js
+++ js/script.js
@@ -19,8 +19,8 @@
 var mySong = {
 	"title":"God's Plan",
 	"artist":"Drake",
-	"mp3-url":"https://open.spotify.com/track/2XW4DbS6NddZxRPm5rMCeY?si=y_SG1s1BTMGGrE_dL6OLPQ",
-	"image-url":"http://www.youredm.com/wp-content/uploads/2018/01/scaryhours.jpg",
+	"mp3":"https://open.spotify.com/track/2XW4DbS6NddZxRPm5rMCeY?si=y_SG1s1BTMGGrE_dL6OLPQ",
+	"image":"http://www.youredm.com/wp-content/uploads/2018/01/scaryhours.jpg",
 	"songLength":"3 minutes, 18 seconds",
 	"album": "Scary Hours"
 }
@@ -29,20 +29,20 @@ var myPlayList = [
 	{
 		"title":"24K Magic",
 		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		"mp3":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+		"image":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 	},
 	{
-		"title1":"Sir Duke",
-		"artist1":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"title":"Sir Duke",
+		"artist":"Stevie Wonder",
+		"mp3":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
+		"image":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
 	},
 	{
-		"title2":"Sorry",
-		"artist2":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"title":"Sorry",
+		"artist":"Justin Bieber",
+		"mp3":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
+		"image":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
 	}
 
 ]
@@ -51,15 +51,18 @@ var myPlayList = [
 
 // DOCUMENT READY FUNCTION
 $( document ).ready(function() {
-  $('body').append("<p>Song Title " + myPlayList.title + "</p>");
-  $('body').append("<p>Artist " + myPlayList.artist + "</p>");
+  $('body').append("<p>Song Title " + mySong.title + "</p>");
+  $('body').append("<p>Artist " + mySong.artist + "</p>");
+  $('body').append("<p>Artist " + mySong.image + "</p>");
 
-
+displayList();
 
 });
 
 function displayList(){
-
+for( var i=0; i< myPlayList.length; i++){
+ $(".songs").append(myPlayList[i].title + "<br>" + myPlayList[i].artist + "<br>" + myPlayList[i].mp3);   
+}
 
   
 }
